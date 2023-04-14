0541d2a2c56e84bb82bad27e9589f403d1794160
LucasRiggi <lucasriggi22@gmail.com>
2023-04-14 15:40:57 -0400

    #7 | Une chaîne avec 10 espaces est invalide

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -23,56 +23,45 @@ describe('ProblemeComponent', () => {
   //   expect(component).toBeTruthy();
   // });
 
-  it('Zone prénom invalide avec 2 caracteres', () => {
+  it('1 | Zone prénom invalide avec 2 caracteres', () => {
     let zone = component.problemeForm?.controls['prenom']
     zone?.setValue('a'.repeat(2))
     let errors = zone?.errors || {};
         expect(errors['minlength']).toBeTruthy();
   });
 
-  it('Zone PRÉNOM valide avec 3 caractères', () => {
+  it('2 | Zone PRÉNOM valide avec 3 caractères', () => {
     let zone = component.problemeForm?.controls['prenom']
     zone?.setValue('a'.repeat(3))
     let errors = zone?.errors || {};
     expect(errors['minlength']).toBeFalsy();
   });
 
-  it('Zone PRÉNOM valide avec 200 caractères', () => {
+  it('3 | Zone PRÉNOM valide avec 200 caractères', () => {
     let zone = component.problemeForm?.controls['prenom']
     zone?.setValue('a'.repeat(200))
     let errors = zone?.errors || {};
     expect(errors['minlength']).toBeFalsy();
   });
 
-  it('Zone PRÉNOM invalide avec aucune valeur', () => {
+  it('4 | Zone PRÉNOM invalide avec aucune valeur', () => {
     let zone = component.problemeForm?.controls['prenom']
     let errors = zone?.errors || {};
     expect(errors['required']).toBeTruthy();
   });
 
-  it('Zone PRÉNOM valide avec 10 espaces', () => {
+  it('5 | Zone PRÉNOM valide avec 10 espaces', () => {
     let zone = component.problemeForm?.controls['prenom']
     zone?.setValue(' '.repeat(10))
     let errors = zone?.errors || {};
     expect(errors['minlength']).toBeFalsy();
   });
 
-  it('Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
+  it('6 | Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
     let zone = component.problemeForm?.controls['prenom']
     zone?.setValue(' '.repeat(2),'a'.repeat(1))
     let errors = zone?.errors || {};
     expect(errors['minlength']).toBeTruthy();
   });
-
-
-
-
-  
-
-  
-
-  
-
-
 
 });
